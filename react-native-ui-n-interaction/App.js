80b87f58029452bf55ed1c19c4aefb5905347a66
Jamesnl55555 <Jamesnl55555@gmail.com>
2026-02-13 04:29:32 +0800

    Done with flex dimensions section

--- react-native-ui-n-interaction/App.js
+++ react-native-ui-n-interaction/App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 // import LotsOfStyles from './components/LotsOfStyles';
-import FixedDimensionsBasics from './components/FixedDimensionsBasics';
+// import FixedDimensionsBasics from './components/FixedDimensionsBasics';
+import FlexDimensionsBasics from './components/FlexDimensionsBasics';
 import { View } from 'react-native';
 
 export default function App() {
   return (
     <View>
       {/* <LotsOfStyles /> */}
-      <FixedDimensionsBasics />
+      {/* <FixedDimensionsBasics /> */}
+      <FlexDimensionsBasics />
       <StatusBar style="auto" />
     </View>
   );
