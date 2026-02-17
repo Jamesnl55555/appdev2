702f302a7dd808f73e055dbdc62374f2f6ed9401
Jamesnl55555 <Jamesnl55555@gmail.com>
2026-02-18 04:27:31 +0800

    Done with align content section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -5,7 +5,8 @@ import { View } from 'react-native';
 // import DirectionLayout from './components/DirectionLayout';
 // import JustifyContentBasics from './components/JustifyContentBasics';
 // import AlignItemsLayout from './components/AlignItemsLayout';
-import AlignSelfLayout from './components/AlignSelfLayout';
+// import AlignSelfLayout from './components/AlignSelfLayout';
+import AlignContentLayout from './components/AlignContentLayout';
 
 export default function App() {
   return (
@@ -15,7 +16,8 @@ export default function App() {
       {/* <DirectionLayout /> */}
       {/* <JustifyContentBasics /> */}
       {/* <AlignItemsLayout /> */}
-      <AlignSelfLayout />
+      {/* <AlignSelfLayout /> */}
+      <AlignContentLayout />
       <StatusBar style="auto" />
     </View>
   );
